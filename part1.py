4c7e41377acd622aeab05da4c2a0fc658305746a
Adrish Mitra <adrishmitra710@gmail.com>
2023-03-21 14:27:41 +0530

    comment added

--- part1.py
+++ part1.py
@@ -5,7 +5,7 @@ import csv
 baseUrl = "https://www.amazon.in"
 url = "https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_1"
 
-def getProducts(url):
+def getSoup(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
     return soup
@@ -15,11 +15,11 @@ def writeProduct(soup):
     prodList = soup.find_all(
         'div', class_="sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16")
 
-    # print(prodList)
-
     for item in prodList:
         url = item.find('a', class_="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")['href']
         name = item.find('span', class_="a-size-medium a-color-base a-text-normal").text
+
+        #If no price, rating or reviews found, set it to N/A
         try:
             price = item.find('span', class_="a-price-whole").text
         except:
@@ -55,7 +55,7 @@ with open('products.csv', 'w', encoding='utf8', newline='') as f:
     writer.writerow(header)
     while True:
         print(url)
-        soup = getProducts(url)
+        soup = getSoup(url)
         writeProduct(soup)
         url = getNextUrl(soup)
         if(not url):
